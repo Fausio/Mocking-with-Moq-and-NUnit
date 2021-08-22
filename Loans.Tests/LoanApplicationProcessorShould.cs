5a6abbbe43546de1cd43f493d52c4ea05e8fd1a8
Fáusio <luisfausio@gmail.com>
2021-08-22 22:04:44 +0200

    verify method with paramiters

--- Loans.Tests/LoanApplicationProcessorShould.cs
+++ Loans.Tests/LoanApplicationProcessorShould.cs
@@ -143,6 +143,38 @@ namespace Loans.Tests
         }
 
 
+        [Test]
+        public void CalculateScore()
+        {
+            LoanProduct product = new LoanProduct(99, "Loan", 5.25m);
+            LoanAmount amount = new LoanAmount("USD", 200_000);
+            var application = new LoanApplication(42,
+                                                  product,
+                                                  amount,
+                                                  "Sarah",
+                                                  25,
+                                                  "133 Pluralsight Drive, Draper, Utah",
+                                                  65_000);
+
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+
+            mockIdentityVerifier.Setup(x => x.Validate("Sarah",
+                                                        25,
+                                                        "133 Pluralsight Drive, Draper, Utah"))
+                                .Returns(true);
+
+            var mockCreditScorer = new Mock<ICreditScorer>();
+            mockCreditScorer.Setup(x => x.ScoreResult.ScoreValue.Score).Returns(300);
+
+
+            var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object,
+                                                   mockCreditScorer.Object);
+
+            sut.Process(application);
+            mockCreditScorer.Verify(x => x.CalculateScore(application.GetApplicantName(), application.GetApplicantAddress()));
+        }
+
+
     }
 }
 
